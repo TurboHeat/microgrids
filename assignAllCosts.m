1c9921138d376814f4b75745fa340f487ef46084
Dev-iL <Gid.CTO+github@gmail.com>
2020-08-12 12:16:22 +0300

    Refactoring assignAllCosts including several minor modifications
    - Hard-coded values turned into inputs or named constants.
    - Most constants were moved to the beginning.
    - Some variables renamed to javaCase.
    - loop indices renamed from `i`.
    - `length` replaced with `size`.
    - Clarified weird variable renaming step + added an assertion that the correct data was indeed loaded.
    - Added a progressbar.
    -`save` syntax made more OS-agnostic.
    - Changed how the output is stored.

--- assignAllCosts.m
+++ assignAllCosts.m
@@ -11,22 +11,48 @@
 % index in the row.
 % Also runs Iliya's mapping to save all needed variables in one file.
 %--------------------------------------------------------------%
-function [decided_costs] = assignAllCosts(showPlot)
+function [varargout] = assignAllCosts(kwargs)
+%% Handle inputs
 arguments
-  showPlot (1,1) logical = false %boolean to decide if the plots of tariffs and demands are shown
+  kwargs.showPlot (1,1) logical = false % will plots of tariffs and demands be shown?
+  kwargs.smoothDemand (1,1) logical = true; % will smoothing be performed on the demand data?
+  kwargs.smoothTime (1,1) double {mustBeInteger, mustBePositive} = 21; % number of steps for smoothing. 15=3.75min, 21=5.25min
+  kwargs.endTime (1,1) double {mustBePositive} = 24; % [h]
 end
-savepath = '..\Data\';
-addpath(genpath(savepath));
+% Unpack kwargs:
+showPlot = kwargs.showPlot;
+smoothDemand = kwargs.smoothDemand;
+smoothTime = kwargs.smoothTime;
+endTime = kwargs.endTime;
 
-%%
-smooth_demand = 1; % decide to perform or not smoothing on the demand data
-smooth_time = 21; %number of steps for smoothing, needs to be odd. 15=3.75min, 21=5.25min
-dt = 15;
-n_lines = 3600 / dt; %number of time steps in 1h
-end_time = 24; %in hours
-T = end_time * n_lines; % total number of time steps in a day
+%% Constants
+savepath = '..\Data\';
+SECONDS_PER_MINUTE = 60;
+MINUTES_PER_HOUR = 60;
+SECONDS_PER_HOUR = SECONDS_PER_MINUTE * MINUTES_PER_HOUR;
+
+% Natural gas parameter
+Qr = 49736500;   % [J/kg]
+h_env = 3.015e5; % [J/kg]
+h_100 = 3.9748e5;% [J/kg]
+eta_HRU = 0.89;
+eta_b = 0.98;
+mair_mf = 17.2 * 1.2;
+Ph_mf = eta_HRU * (mair_mf + 1) * ((Qr * eta_b + mair_mf * h_env) / (mair_mf + 1) - h_100) / 3.6e6; %kWh/kg
+price_ft3 = [7.74, 8.85, 6.80]; % $/1000ft^3
+density_CH4 = 0.68; % kg/m^3
+ft3_m3 = power(0.3048, 3);
+price_m3 = price_ft3 ./ (1000 * ft3_m3); %price in $/m^3
+price_kg_f = price_m3 / density_CH4; % for MGT costs
+price_kWh = price_kg_f / Ph_mf; % price in $/kWh, for heat tariff
+heat_tariff = price_kWh;
+
+%
+dt = 15; % duration of time step in [s]
+n_lines = SECONDS_PER_HOUR / dt; % number of time steps in 1h
+T = endTime * n_lines; % total number of time steps
 BUILDING = [1, 2, 3, 4];
-DAY      = [1 2 3];
+DAY      = [1, 2, 3];
 buildingstring = {'Large Hotel', 'Full Service Restaraunt', 'Small Hotel', 'Residential'}; %for plotting
 daystring = {'Winter', 'Spring/Autumn', 'Summer'}; %for plotting
 
@@ -36,29 +62,28 @@ tariff_map = uint8([1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4; 1, 2, 3, 1, 2, 3, 1, 2, 
 elec_tariff = zeros(T, numel(BUILDING)*numel(DAY));
 power_demand = zeros(T, numel(BUILDING)*numel(DAY));
 heat_demand = zeros(T, numel(BUILDING)*numel(DAY));
-i = 1;
+q = 1;
 for b = 1:length(BUILDING)
   for d = 1:length(DAY)
-    elec_tariff(:, i) = createElectricityTariffProfile(b, d, dt);
-    [power_demand(:, i), heat_demand(:, i)] = createDemandProfileVector(b, d, dt);
+    elec_tariff(:, q) = createElectricityTariffProfile(b, d, dt);
+    [power_demand(:, q), heat_demand(:, q)] = createDemandProfileVector(b, d, dt);
     % SMOOTHING
-    if smooth_demand
-      power_demand(:, i) = smooth(power_demand(:, i), smooth_time); %select appropriate amount of hours
-      heat_demand(:, i) = smooth(heat_demand(:, i), smooth_time);
+    if smoothDemand
+      power_demand(:, q) = smooth(power_demand(:, q), smoothTime); %select appropriate amount of hours
+      heat_demand(:, q) = smooth(heat_demand(:, q), smoothTime);
     end
-    i = i + 1;
+    q = q + 1;
   end
 end
-%heat_tariff=0.04; %define heat tariff
 
 %% plotting according to building type
 if showPlot
-  t = (1:n_lines * end_time).' ./ n_lines; % what is it?
+  t = (1:n_lines * endTime).' ./ n_lines; % what is it?
   %Plot tariffs
   k = 1;
-  for i = 1:4 %building
-    figure(i);
-    suptitle([buildingstring{i}; {''}; {''}]); %empty chars to get correct spacing
+  for q = 1:4 %building
+    figure(q);
+    suptitle([buildingstring{q}; {''}; {''}]); %empty chars to get correct spacing
     for j = 1:3 %day
       subplot(1, 3, j);
       plot(t, elec_tariff(:, k));
@@ -71,9 +96,9 @@ if showPlot
   end
   %Plot demands
   k = 1;
-  for i = 1:4 %building
-    figure(i+4);
-    suptitle([buildingstring{i}; {''}; {''}]); %empty chars to get correct spacing
+  for q = 1:4 %building
+    figure(q+4);
+    suptitle([buildingstring{q}; {''}; {''}]); %empty chars to get correct spacing
     for j = 1:3 %day
       subplot(1, 3, j);
       plot(t, power_demand(:, k), t, heat_demand(:, k));
@@ -88,44 +113,32 @@ if showPlot
 end
 
 %% Run Iliya's mapping and save all the variables at once
-load graph_24h.mat 'g' 'svToStateNumber';
+load('graph_24h.mat', 'g', 'svToStateNumber');
 state_from = g.Edges.EndNodes(:,1);
 state_to = g.Edges.EndNodes(:, 2);
 
 %%%
-total_nodes = length(svToStateNumber); %smax*(vmax+1)+1
-[SV_states, time_from, n_tsteps, from_state_map, to_state_map] = transformAdjacencyMatrix(state_from, state_to, svToStateNumber);
+total_nodes = size(svToStateNumber, 1); %(smax-smin)*(vmax-vmin+1)+1
+[SV_states, time_from, n_tsteps, from_state_map, to_state_map] = ...
+  transformAdjacencyMatrix(state_from, state_to, svToStateNumber);
 clear A g;
-% Load (and optionally rename) mappings
-% the file may have different contents (!?), so we rename the variables
-load('sv_mappings') %#ok<LOAD>
-if ~exist('power_map', 'var')
-  power_map = PowerSurf; clear PowerSurf
+%% Load (and optionally rename) mappings
+if isfile(fullfile(savepath, 'CHP.mat')) % Engine similar to Capstone C65 (incl. extrapolation)
+  tmp = load(fullfile(savepath, 'CHP.mat'));
+  power_map = tmp.PowerSurf;
+  heat_map = tmp.PheatSurfExt;
+  m_dot_f_map = tmp.FFSurfExt;
+else
+  tmp = load(fullfile(savepath, 'sv_mappings.mat')); % "Virtual" engine
+  power_map = tmp.power_map;
+  heat_map = tmp.heat_map;
+  m_dot_f_map = tmp.m_dot_f_map;
 end
-if ~exist('heat_map', 'var')
-  heat_map = PheatSurfExt; clear PheatSurfExt
-end
-if ~exist('m_dot_f_map', 'var')
-  m_dot_f_map = FFSurfExt; clear FFSurfExt
-end
-
-%% Natural gas parameter
-Qr = 49736500; %J/kg
-h_env = 3.015e5; %J/kg
-h_100 = 3.9748e5; %J/kg
-eta_HRU = 0.89;
-eta_b = .98;
-mair_mf = 17.2 * 1.2;
-Ph_mf = eta_HRU * (mair_mf + 1) * ((Qr * eta_b + mair_mf * h_env) / (mair_mf + 1) - h_100) / 3.6e6; %kWh/kg
-price_ft3 = [7.74, 8.85, 6.80]; % $/1000ft^3
-density_CH4 = 0.68; % kg/m^3
-ft3_m3 = power(0.3048, 3);
-price_m3 = price_ft3 ./ (1000 * ft3_m3); %price in $/m^3
-price_kg_f = price_m3 / density_CH4; % for MGT costs
-price_kWh = price_kg_f / Ph_mf; % price in $/kWh, for heat tariff
-heat_tariff = price_kWh;
-
-%% Reshape power maps
+clear tmp
+% Verify that we have the right mappings:
+assert( isequal( numel(power_map), numel(heat_map), numel(m_dot_f_map), total_nodes-1 ), ...
+  'Mismatch between matrix sizes! Try using a different ''sv_mappings'' file.');
+%% Reshape the power maps
 power_map = [reshape(power_map.', [], 1); 0];
 heat_map = [reshape(heat_map.', [], 1); 0];
 fuel_map = [reshape(m_dot_f_map.', [], 1); 0];
@@ -144,22 +157,34 @@ transition_penalty = [zeros(total_nodes, 1); ...
 %% Main loop to assign edges
 k = 1;
 decided_costs = zeros(length(to_state_map), numel(BUILDING)*numel(DAY)*numel(price_kg_f));
-for cost = 1:numel(price_kg_f)
+progressbar('Price levels [kg*f]','Building*day combinations');
+nPrices = numel(price_kg_f);
+nConfigs = numel(BUILDING) * numel(DAY);
+for cost = 1:nPrices % this takes ~4min
   fuel_price = price_kg_f(cost);
-  for i = 1:numel(BUILDING) * numel(DAY)
-    decided_costs(:, k) = assignCosts(double(dt), power_map, heat_map, fuel_map, mdot_fuel_SU, total_nodes, sol_select, time_from, n_tsteps, from_state_map, to_state_map, power_demand(:, i), heat_demand(:, i), elec_tariff(:, i), heat_tariff(cost), fuel_price, transition_penalty);
+  for q = 1:nConfigs
+    decided_costs(:, k) = assignCosts(double(dt), power_map, heat_map, fuel_map, mdot_fuel_SU, total_nodes, sol_select, time_from, n_tsteps, from_state_map, to_state_map, power_demand(:, q), heat_demand(:, q), elec_tariff(:, q), heat_tariff(cost), fuel_price, transition_penalty);
     k = k + 1;
+    progressbar([], q/nConfigs);
   end
+  progressbar(cost/nPrices, []);
 end
+progressbar(1); % close progressbar
 
-%%
+%% Save results to disk
+% This requires ~3.4GB of space!
 aux = decided_costs;
 decided_costs = single(decided_costs);
-save([savepath, 'all_data.mat'], 'tariff_map', 'elec_tariff', 'power_demand', ...
+save(fullfile(savepath, 'all_data.mat'), 'tariff_map', 'elec_tariff', 'power_demand', ...
   'heat_demand', 'SV_states', 'time_from', 'n_tsteps', 'heat_tariff', ...
   'from_state_map', 'to_state_map', 'heat_map', 'm_dot_f_map', 'power_map', ...
   'price_kg_f', 'decided_costs', 'state_from', 'state_to', 'sol_select');
 
 %g1=digraph(state_from, state_to, decided_costs(:,1));
-save([savepath, 'graph_data_all_days.mat'], '-regexp', '[^aux]');
-decided_costs = aux;
+save(fullfile(savepath, 'graph_data_all_days.mat'), '-regexp', '[^aux]');
+% Assign output:
+if nargout
+  varargout{1} = aux;
+else
+  assignin('base', 'decided_costs', aux);
+end
