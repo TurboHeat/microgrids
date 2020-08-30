f7527f7c69f6c98e6c66bf2a2e356d4232455ad0
Dev-iL <Gid.CTO+github@gmail.com>
2020-08-30 12:41:42 +0300

    Improved documentation in createDemandProfileVector
    + Removed unnecessary commented-out code.
    + Moved function signature to the top.

--- CreateDemandProfileVector.m
+++ CreateDemandProfileVector.m
@@ -1,50 +1,51 @@
-
-%% Function that creates the demand vectors for power and heat in Wh
-% Buildingtypes:
-%           1. Large Hotel
-%           2. Full Service Restaurant
-%           3. Small Hotel
-%           4. Residential High
-%           5. Hospital
-% Days:     1. - 10.th January
-%           2. - 10.th April
-%           3. - 10.th July
-%           4. - 10.th October
-% Stepping of vector is defined with dt
 function [power_return, heat_return] = createDemandProfileVector(buildingtype, day, dt)
+%% Function that creates the demand vectors for power and heat in Wh
+%{
+Buildingtypes:
+          1. Large Hotel
+          2. Full Service Restaurant
+          3. Small Hotel
+          4. Residential High
+          5. Hospital
+Days:     1. - 10.th January
+          2. - 10.th April
+          3. - 10.th July
+          4. - 10.th October
+Stepping of vector is defined with dt
+
+Example of converting .csv data to what is found in the .mat files for the "Hospital" case:
+1) Open RefBldgHospitalNew2004_v1.3_7.1_4A_USA_MD_BALTIMORE.csv
+2) power = L218:L241; heat = M218:M241 + 0.7*N218:N241;
+3) (repeat stage 2 for the three other example days)
+%}
 n_lines = 3600 / dt;
 n_residential = 20;
 power = [];
 heat = [];
 switch buildingtype
   case 1 % Large Hotel
-    load('Demand Profiles\DemandLargeHotel.mat')
-    %load('DemandLargeHotel.mat')
-    %         load('Demand Profiles/DemandLargeHotel.mat') % mac syntax
+    load('../Data/Demand Profiles/DemandLargeHotel.mat')
     power_return(1, :) = power(:, day);
     heat_return(1, :) = heat(:, day);
   case 2 % Full Service Restaurant
-    load('Demand Profiles\DemandFSR.mat')
-    %         load('Demand Profiles/DemandFSR.mat') % mac syntax
+    load('../Data/Demand Profiles/DemandFSR.mat')
     power_return(1, :) = power(:, day);
     heat_return(1, :) = heat(:, day);
   case 3 % Small Hotel
-    load('Demand Profiles\DemandSmallHotel.mat')
-    %         load('Demand Profiles/DemandSmallHotel.mat') % mac syntax
+    load('../Data/Demand Profiles/DemandSmallHotel.mat')
     power_return(1, :) = power(:, day);
     heat_return(1, :) = heat(:, day);
   case 4 % Residential One Unit * n_residential
-    load('Demand Profiles\DemandResidentialOneUnit.mat')
-    %         load('Demand Profiles/DemandResidentialOneUnit.mat') % mac syntax
+    load('../Data/Demand Profiles/DemandResidentialOneUnit.mat')
     power_return(1, :) = power(:, day) * n_residential;
     heat_return(1, :) = heat(:, day) * n_residential;
   case 5 % Hospital
-    load('Demand Profiles\DemandHospital.mat')
-    %         load('Demand Profiles/DemandHospital.mat') % mac syntax
+    load('../Data/Demand Profiles/DemandHospital.mat', 'power', 'heat')
     power_return(1, :) = power(:, day);
     heat_return(1, :) = heat(:, day);
 end
 for line = 2:n_lines
+  % TODO: use repelem/repmat, or modify calling code to access elements using floor(t/dt) or interp1
   power_return(line, :) = power_return(1, :); % copy values 240 times, i.e. each time step in an hour has the same demand
   heat_return(line, :) = heat_return(1, :);
 end
