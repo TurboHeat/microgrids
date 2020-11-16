c91d0fbaed89db5ddecd91c3cab73c9c99d6170a
Dev-iL <Gid.CTO+github@gmail.com>
2020-11-16 15:04:32 +0200

    `runAlgorithms` turned into a function (was script) + other changes
    + Renamed some variables to keep with the convention.
    + `buildingTypes` and `priceIndices` are no longer hardcoded, and instead obtained from the function `getCaseIndices()`
    + Algorithm output inside the `parfor` is first saved to a temporaty variable `out`, and only then put into the `outputData` struct. This was done in preparation for saving of individual iteration results.
    + Fixes some indentation and spacing.

--- runAlgorithms.m
+++ runAlgorithms.m
@@ -1,3 +1,4 @@
+function [allScenariosData] = runAlgorithms(fuelIdx)
 %% Choose Running Algorithms
 runBenchmark = 1;
 
@@ -7,17 +8,17 @@ runRobustLinfty = 1;
 LinftyAlphas = [0.01:0.01:0.05, 0.1:0.05:0.7];
 
 runRobustMixed = 1;
-MixedAlphas = repmat([0.01:0.01:0.05,0.07,0.1,0.13,0.15,0.17,0.2],[1,10]);
-MixedSpikeAlphas = [.5*sqrt(5760)*ones(1,11),...
-                    1*sqrt(5760)*ones(1,11),...
-                    1.5*sqrt(5760)*ones(1,11),...
-                    2*sqrt(5760)*ones(1,11),...
-                    2.5*sqrt(5760)*ones(1,11),...
-                    3*sqrt(5760)*ones(1,11),...
-                    3.5*sqrt(5760)*ones(1,11),...
-                    4*sqrt(5760)*ones(1,11),...
-                    1*5760*ones(1,11),...
-                    2*5760*ones(1,11)];
+mixedAlphas = repmat([0.01:0.01:0.05,0.07,0.1,0.13,0.15,0.17,0.2],[1,10]);
+mixedSpikeAlphas = [.5*sqrt(5760)*ones(1,11),...
+  1*sqrt(5760)*ones(1,11),...
+  1.5*sqrt(5760)*ones(1,11),...
+  2*sqrt(5760)*ones(1,11),...
+  2.5*sqrt(5760)*ones(1,11),...
+  3*sqrt(5760)*ones(1,11),...
+  3.5*sqrt(5760)*ones(1,11),...
+  4*sqrt(5760)*ones(1,11),...
+  1*5760*ones(1,11),...
+  2*5760*ones(1,11)];
 
 %% Choose Prices and Building Combinations
 %i-th scenario has building type BuildingTypes(i) and the index of the gas
@@ -25,103 +26,115 @@ MixedSpikeAlphas = [.5*sqrt(5760)*ones(1,11),...
 %
 %The building type should be in the set {1,2,3,4}, and the price index
 %should be in the set {1 ... numFuelPrices}
-BuildingTypes = [1,2,3,4,1,2,3,4,1,2,3,4];
-PriceIndices  = [1,1,1,1,2,2,2,2,3,3,3,3];
+BUILDINGS_TO_TEST = BuildingType(1:4);
+nBuildTypes = numel(BUILDINGS_TO_TEST);
+nFuelPrices = numel( NATURAL_GAS_PARAMS() );
+[buildingTypes, priceIndices] = getCaseIndices(nBuildTypes, nFuelPrices);
 
 %% Assert and initilize variables
 
-assert(numel(BuildingTypes) == numel(PriceIndices), ...
-       'Incompatible number of building types and gas price indices.');
-   
-assert(numel(MixedAlphas) == numel(MixedSpikeAlphas), ...
-       'Incompatible number of parameters for robust mixed algorithm.');
-   
-numScenarios = numel(BuildingTypes);
+assert(numel(buildingTypes) == numel(priceIndices), ...
+  'Incompatible number of building types and gas price indices.');
+
+assert(numel(mixedAlphas) == numel(mixedSpikeAlphas), ...
+  'Incompatible number of parameters for robust mixed algorithm.');
+
+numScenarios = numel(buildingTypes);
 numRobustLinfty = numel(LinftyAlphas);
-numRobustMixed = numel(MixedAlphas);
+numRobustMixed = numel(mixedAlphas);
 
 % Prepare arrays to allow a single itertor to go over all
 % algorithm-scenario pairs.
-ScenarioList = 1:numScenarios;
-AlgorithmList = 1:(1*runBenchmark + 1*runNominal + numRobustLinfty*runRobustLinfty + numRobustMixed *runRobustMixed);
-numAlgorithms = numel(AlgorithmList);
+scenarioList = 1:numScenarios;
+algorithmList = 1:(1*runBenchmark + 1*runNominal + numRobustLinfty*runRobustLinfty + numRobustMixed *runRobustMixed);
+numAlgorithms = numel(algorithmList);
 
-[Scenarios,Algorithms] = meshgrid(ScenarioList,AlgorithmList);
-numPairs = numel(Scenarios);
-Scenarios = reshape(Scenarios,[numPairs,1]);
-Algorithms = reshape(Algorithms,[numPairs,1]);
+[scenarios, algorithms] = meshgrid(scenarioList, algorithmList);
+numPairs = numel(scenarios);
+scenarios = reshape(scenarios,[numPairs,1]);
+algorithms = reshape(algorithms,[numPairs,1]);
 
 %-1 = Benchmark. 0 = Nominal. 1 = Robust Linfty.  2 = Robust Mixed.
-AlgorithmTypeKey = [-ones(runBenchmark,1);zeros(runNominal,1);ones(numRobustLinfty*runRobustLinfty,1);2*ones(numRobustMixed *runRobustMixed,1)];
-AlgorithmType = AlgorithmTypeKey(Algorithms);
+algorithmTypeKey = [-ones(runBenchmark,1);
+                     zeros(runNominal,1);
+                     ones(numRobustLinfty*runRobustLinfty,1);
+                     2*ones(numRobustMixed *runRobustMixed,1)];
+algorithmType = algorithmTypeKey(algorithms);
 
-AlgorithmParameterKey = [1:runBenchmark,1:runNominal,1:numRobustLinfty*runRobustLinfty,1:numRobustMixed *runRobustMixed]';
-AlgorithmParameters = AlgorithmParameterKey(Algorithms);
+algorithmParameterKey = [1:runBenchmark,1:runNominal,1:numRobustLinfty*runRobustLinfty,1:numRobustMixed *runRobustMixed]';
+algorithmParameters = algorithmParameterKey(algorithms);
 
 % Prepare structure for output data
-OutputData(numPairs).Data.Power_Generation = [];
-OutputData(numPairs).Data.Heat_Generation = [];
-OutputData(numPairs).Data.Fuel_Consumption = [];
-OutputData(numPairs).Data.EstimatedCost = [];
-OutputData(numPairs).Data.TrueCost = [];
-OutputData(numPairs).Data.AlgorithmType = [];
-OutputData(numPairs).Data.AlgorithmParameters{1} = [];
+outputData(numPairs).Data.Power_Generation = [];
+outputData(numPairs).Data.Heat_Generation = [];
+outputData(numPairs).Data.Fuel_Consumption = [];
+outputData(numPairs).Data.EstimatedCost = [];
+outputData(numPairs).Data.TrueCost = [];
+outputData(numPairs).Data.AlgorithmType = [];
+outputData(numPairs).Data.AlgorithmParameters{1} = [];
 
-OutputData(numPairs).BuildingType = 0;
-OutputData(numPairs).PriceIndex = 0;
+outputData(numPairs).BuildingType = 0;
+outputData(numPairs).PriceIndex = 0;
 %% Run Algorithms on Scenarios
 
-parfor iter = 1:numel(Scenarios)
-    jScenario = Scenarios(iter);
-    iAlgorithm = Algorithms(iter);
-    AlgType = AlgorithmType(iter);
-    AlgParam = AlgorithmParameters(iter);
-    
-    Building = BuildingTypes(jScenario);
-    PriceInd = PriceIndices(jScenario);
-    
-    switch AlgType
-        case -1
-            OutputData(iter).Data = runBenchmarkAlgorithm('PriceIndex',PriceInd,...
-                'BuildingType',Building);
-        case 0
-            OutputData(iter).Data = runNominalAlgorithm('PriceIndex',PriceInd,...
-                'BuildingType',Building);
-        case 1
-            OutputData(iter).Data = runRobustLinftyAlgorithm('PriceIndex',PriceInd,...
-                'BuildingType',Building,...
-                'alpha',LinftyAlphas(AlgParam));
-        case 2
-            OutputData(iter).Data = runRobustMixedAlgorithm('PriceIndex',PriceInd,...
-                'BuildingType',Building,...
-                'alphaMixed',MixedAlphas(AlgParam),...
-                'alphaSpikes',MixedSpikeAlphas(AlgParam)); 
-    end
-    
-    OutputData(iter).BuildingType = Building;
-    OutputData(iter).PriceIndex = PriceInd;
+parfor iter = 1:numel(scenarios)
+  jScenario = scenarios(iter);
+  iAlgorithm = algorithms(iter);
+  AlgType = algorithmType(iter);
+  AlgParam = algorithmParameters(iter);
+  
+  building = buildingTypes(jScenario);
+  priceInd = priceIndices(jScenario);
+  
+  switch AlgType
+    case -1
+      out = runBenchmarkAlgorithm('PriceIndex',priceInd,...
+        'BuildingType', building);
+    case 0
+      out = runNominalAlgorithm('PriceIndex',priceInd,...
+        'BuildingType', building);
+    case 1
+      out = runRobustLinftyAlgorithm('PriceIndex',priceInd,...
+        'BuildingType', building,...
+        'alpha', LinftyAlphas(AlgParam));
+    case 2
+      out = runRobustMixedAlgorithm('PriceIndex',priceInd,...
+        'BuildingType',building,...
+        'alphaMixed', mixedAlphas(AlgParam),...
+        'alphaSpikes', mixedSpikeAlphas(AlgParam));
+  end
+  
+  outputData(iter).Data = out;
+  outputData(iter).BuildingType = building;  
+  outputData(iter).PriceIndex = priceInd;
 end
 
 %% Parse to a new form, which is easier to analyze
 % Initialize Structure - the values have no meaning, only the data
 % structure.
-CurrentScenarioData(numAlgorithms) = OutputData(1).Data;
-AllScenariosData(numScenarios).Data = CurrentScenarioData;
-AllScenariosData(numScenarios).BuildingType = 0;
-AllScenariosData(numScenarios).PriceIndex = 0;
+currentScenarioData(numAlgorithms) = outputData(1).Data;
+allScenariosData(numScenarios).Data = currentScenarioData;
+allScenariosData(numScenarios).BuildingType = 0;
+allScenariosData(numScenarios).PriceIndex = 0;
 
 for jScenario = 1:numScenarios
-    Building = BuildingTypes(jScenario);
-    PriceInd = PriceIndices(jScenario);
-    k = 1;
-    for iter = 1:numPairs
-        if(Building == OutputData(iter).BuildingType && ...
-           PriceInd == OutputData(iter).PriceIndex)
-       CurrentScenarioData(k) = OutputData(iter).Data;
-       k = k+1;
-        end
+  building = buildingTypes(jScenario);
+  priceInd = priceIndices(jScenario);
+  k = 1;
+  for iter = 1:numPairs
+    if (building == outputData(iter).BuildingType) && ...
+       (priceInd == outputData(iter).PriceIndex)
+      currentScenarioData(k) = outputData(iter).Data;
+      k = k+1;
     end
-    AllScenariosData(jScenario).Data = CurrentScenarioData;
-    AllScenariosData(jScenario).BuildingType = Building;
-    AllScenariosData(jScenario).PriceIndex = PriceInd;
+  end
+  allScenariosData(jScenario).Data = currentScenarioData;
+  allScenariosData(jScenario).BuildingType = building;
+  allScenariosData(jScenario).PriceIndex = priceInd;
+end
+end
+
+function [buildIdx, priceIdx] = getCaseIndices(nBuildings, nFuelPrices)
+buildIdx = repmat(1:nBuildings, 1, nFuelPrices);
+priceIdx = repelem(1:nFuelPrices, 1, nBuildings);
 end
